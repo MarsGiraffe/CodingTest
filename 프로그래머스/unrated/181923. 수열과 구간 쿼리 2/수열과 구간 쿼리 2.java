4728206f8d43b0946174ab4d42af6756690d3f8e
Kim DongHyun <139114497+MarsGiraffe@users.noreply.github.com>
2023-10-06 13:39:00 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 4.15 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        for(int i=0; i<queries.length; i++){
+            for(int j=queries[i][0]; j<=queries[i][1]; j++){
+                if(arr[j]>queries[i][2]){
+                    answer[i] = answer[i]==0?arr[j]: answer[i]<arr[j]? answer[i]:arr[j];
+                }
+            }
+            if(answer[i]==0) answer[i] = -1;
+        }
+        
+        return answer;
+    }
+}
